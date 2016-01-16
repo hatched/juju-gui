375a4f84b329061fee4abfd3ff3b2d1788387b2a
Jeff Pihach <jeff.pihach@canonical.com>
2016-01-16 00:45:40 -0600

    create top level imports.js to properly build bundle rollup

---
+++ jujugui/static/gui/src/app/imports.js
@@ -0,0 +1,9 @@
+import AddedServicesList from './components/added-services-list/added-services-list'
+import Panel from './components/panel/panel'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+juju.components.AddedServicesList = AddedServicesList
+juju.components.Panel = Panel
+window.React = React
+window.ReactDOM = ReactDOM
